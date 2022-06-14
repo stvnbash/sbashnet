e7a4c2731d051029fe55736c322e4d7f6d26b267
Steven Bash <56286042+stvnbash@users.noreply.github.com>
2022-06-13 23:51:44 -0700

    Started working on home page and intro component

---
+++ components/Intro.tsx
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+import Image from 'next/image'
+
+export default function Intro() {
+    return (
+        <div className='text-center text-white text-5xl font-bold'>
+            Hi, I'm <span className='text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500'>Steven</span>
+        </div>
+    )
+}
