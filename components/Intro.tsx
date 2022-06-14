444fd0800ae28e868fe08d35c402508d6e002bfe
Steven Bash <56286042+stvnbash@users.noreply.github.com>
2022-06-14 00:06:44 -0700

    Uptated Intro component

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -6,7 +6,7 @@ export default function Intro() {
         <div className='pt-10 text-center text-white text-5xl font-bold'>
             Hi, I'm <span className='text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500'>Steven Bash</span>
             <br />
-            UCLA Computer Science Undergraduate
+            UCLA Computer Science
         </div>
     )
 }
