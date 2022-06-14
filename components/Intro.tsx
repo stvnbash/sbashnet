b8c6693d401f5e18d8f5cef6326dd6b786c72870
Steven Bash <56286042+stvnbash@users.noreply.github.com>
2022-06-13 23:53:51 -0700

    Uptated Intro component

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 export default function Intro() {
     return (
         <div className='text-center text-white text-5xl font-bold'>
-            Hi, I'm <span className='text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500'>Steven</span>
+            Hi, I'm <span className='text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500'>Steven Bash</span>
         </div>
     )
 }
