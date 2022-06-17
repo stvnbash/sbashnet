b6ccff7b333737667c5b145b534f78fdeb6cf68f
Steven Bash <56286042+stvnbash@users.noreply.github.com>
2022-06-16 20:47:52 -0700

    Messing with page layout

---
+++ pages/404.tsx
@@ -0,0 +1,7 @@
+export default function NotFoundPage() {
+    return (
+        <div className="flex h-[75vh] items-center justify-center">
+            <p>Hi there, you seem to be lost.</p>
+        </div>
+    )
+}
