f0975fe24a3310e32a3dbc7eda9b53b0156193d8
Steven Bash <56286042+stvnbash@users.noreply.github.com>
2022-06-16 21:11:48 -0700

    Updated Intro

--- components/Intro1.tsx
+++ components/Intro1.tsx
@@ -7,7 +7,8 @@ export default function Intro() {
         <div className=''>
             <p className='text-xl'>Hello, world!  I am</p>
             <h1 className='text-5xl font-bold py-3 text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500'>Steven Bash</h1>
-            <p className='text-xl'>I strive to create impactful change and strategize for organizational efficiency.  I study Computer Science at UCLA.</p>
+            {/* I strive to create impactful change and strategize for organizational efficiency.   */}
+            <p className='text-xl'>I study Computer Science at UCLA.</p>
             <br />
             <Socials />
         </div>
