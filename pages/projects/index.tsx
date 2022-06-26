7915ac7f0a9c1a5bb5d7c848d6896ea4e8704d36
Steven Bash <56286042+stvnbash@users.noreply.github.com>
2022-06-25 20:56:11 -0700

    Starting to build out projects page

---
+++ pages/projects/index.tsx
@@ -0,0 +1,12 @@
+export default function Projects() {
+    return (
+        // <div className="flex h-[75vh] items-center justify-center">
+        <div className="">
+            <p>Projects coming soon! :-)</p>
+            <br />
+            <ul>
+               
+            </ul>
+        </div>
+    )
+}
