153a66a23755980f246eeeec8ee656d14f674f4e
Steven Bash <56286042+stvnbash@users.noreply.github.com>
2022-06-26 00:18:41 -0700

    Set up Contentlayer for projects page

--- pages/projects/[slug].tsx
+++ pages/projects/[slug].tsx
@@ -1,12 +1,59 @@
-export default function Projects() {
-    return (
-        // <div className="flex h-[75vh] items-center justify-center">
-        <div className="">
-            <p>Projects coming soon! :-)</p>
-            <br />
-            <ul>
-               
-            </ul>
-        </div>
-    )
+import Head from 'next/head'
+import Link from 'next/link'
+import { format, parseISO } from 'date-fns'
+import { allPosts } from 'contentlayer/generated'
+import Meta from 'components/Meta'
+
+export async function getStaticPaths() {
+  const paths = allPosts.map((post) => post.url)
+  return {
+    paths,
+    fallback: false,
+  }
 }
+
+export async function getStaticProps({ params }:any) {
+  const post = allPosts.find((post) => post._raw.flattenedPath === params.slug)
+  return {
+    props: {
+      post,
+    },
+  }
+}
+
+export default function PostLayout({ post }:any) {
+  return (
+    <>
+      <Meta title={post.title} />
+      <article className="mx-auto max-w-2xl py-16">
+        <div className="mb-6 text-center">
+          <Link href="/projects">
+            <a className="text-center text-sm font-bold uppercase text-turquoise1">Go Back</a>
+          </Link>
+        </div>
+        <div className="mb-6 text-center">
+          <h1 className="mb-1 text-3xl font-bold">{post.title}</h1>
+          {/* <time dateTime={post.date} className="text-sm text-slate-600">
+            {format(parseISO(post.date), 'LLLL d, yyyy')}
+          </time> */}
+        </div>
+        <div className="cl-post-body" dangerouslySetInnerHTML={{ __html: post.body.html }} />
+      </article>
+    </>
+  )
+}
+
+
+
+// export default function Projects() {
+//     return (
+//         // <div className="flex h-[75vh] items-center justify-center">
+//         <div className="">
+//             <p>Projects coming soon! :-)</p>
+//             <br />
+//             <ul>
+               
+//             </ul>
+//         </div>
+//     )
+// }
--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -1,12 +1,39 @@
-export default function Projects() {
+import Head from 'next/head'
+import Link from 'next/link'
+import { compareDesc, format, parseISO } from 'date-fns'
+import { allPosts } from 'contentlayer/generated'
+
+export async function getStaticProps() {
+    const posts = allPosts.sort((a, b) => {
+        return compareDesc(new Date(a.date), new Date(b.date))
+    })
+    return { props: { posts } }
+}
+
+function PostCard(post: any) {
     return (
-        // <div className="flex h-[75vh] items-center justify-center">
-        <div className="">
-            <p>Projects coming soon! :-)</p>
-            <br />
-            <ul>
-               
-            </ul>
+        <div className="mb-6">
+            {/* <time dateTime={post.date} className="block text-sm text-text1">
+                {format(parseISO(post.date), 'LLLL d, yyyy')}
+            </time> */}
+            <h2 className="text-xl font-bold">
+                <Link href={post.url}>
+                    <a className="text-turquoise1 hover:text-yellow1 transition-all">{post.title}</a>
+                </Link>
+            </h2>
+        </div>
+    )
+}
+
+export default function Projects({ posts }: any) {
+    return (
+        <div className='mb-auto'>
+            <h1 className='text-5xl mb-10 font-bold'>My Projects</h1>
+            <div className="grid grid-cols-1">
+                {posts.map((post: any, idx: any) => (
+                    <PostCard key={idx} {...post} />
+                ))}
+            </div>
         </div>
     )
 }
