30a9eb9734a7320d8eb07ecd4a91c087c19a37b1
Steven Bash <56286042+stvnbash@users.noreply.github.com>
2022-06-26 02:31:18 -0700

    Fixed NextJS/React build errors

--- pages/404.tsx
+++ pages/404.tsx
@@ -5,7 +5,7 @@ export default function NotFoundPage() {
             {/* It's all connected.  Except, you're not connected to the right page.  Can you check the URL? */}
             <p>Hi there, you seem to be lost.  Can you double check the URL?</p>
             <br />
-            <p>Is something broken?  Feel free to open up an <a className="text-red1 hover:text-yellow1 underline underline-offset-2 transition-all" href="https://github.com/stvnbash/sbashnet/issues/new" target="_blank">issue on GitHub</a>!</p>
+            <p>Is something broken?  Feel free to open up an <a className="text-red1 hover:text-yellow1 underline underline-offset-2 transition-all" href="https://github.com/stvnbash/sbashnet/issues/new" target="_blank" rel="noreferrer">issue on GitHub</a>!</p>
         </div>
     )
 }
