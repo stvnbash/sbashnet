3b02636fca6e56f2fc8e5b69add3c263871c380b
Steven Bash <56286042+stvnbash@users.noreply.github.com>
2024-12-02 01:35:34 -0800

    update resumeURL to be embedded version for preview in browser

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 const { withContentlayer} = require('next-contentlayer')
 
-const resumeURL = "https://1drv.ms/b/s!AsvAGOMj9b-tiZ0FaQ68uPVXy2RJFQ?e=Hb4FXL"
+// const resumeURL = "https://1drv.ms/b/s!AsvAGOMj9b-tiZ0FaQ68uPVXy2RJFQ?e=Hb4FXL" // this is a direct url but onedrive isn't showing preview, only download button
+const resumeURL = "https://1drv.ms/b/s!AsvAGOMj9b-tiZ0FOvglVcRMWqO-Jw?embed=1&em=2" // this is a onedrive preview, meant for embedding
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
